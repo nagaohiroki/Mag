0d708a1a1ea997b8c4486bb25b7ccb8ae1292da2
nagaohiroki <testplayers0226@gmail.com>
2018-11-16 14:28:50 +0900

    add unity project

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	Rigidbody mRigidbody;
+	[SerializeField]
+	Transform mCameraHandleTarnsform;
+	void Update()
+	{
+		if(mRigidbody != null)
+		{
+			var vec = Vector3.zero;
+			vec.z = -Input.GetAxis("Horizontal");
+			mRigidbody.AddTorque(vec, ForceMode.VelocityChange);
+			mRigidbody.AddForce(vec, ForceMode.VelocityChange);
+		}
+		if(mCameraHandleTarnsform != null)
+		{
+			mCameraHandleTarnsform.position = transform.position;
+		}
+	}
+	void OnCollisionEnter(Collision inCollision)
+	{
+		if(inCollision.gameObject.tag != "Not")
+		{
+			inCollision.gameObject.transform.SetParent(transform);
+		}
+	}
+}
